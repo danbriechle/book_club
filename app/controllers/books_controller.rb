a4b7fef86469f0fc90c70dc100966053d78e197b
Daniel Briechle <danbriechle@hotmail.com>
2018-12-09 16:42:22 -0700

    Finishes create method for new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,19 +16,31 @@ class BooksController < ApplicationController
     @bottom_three_reviews = Review.bottom_three
     @average_score = Review.average_score
   end
-  
-  # def new
-  #   @book = Book.new
 
-  # end
-  #
-  # def create
-  #   @book = author.books.create(book_params)
-  # end
+  def new
+  @book = Book.new
+  end
+
+  def create
+    book = Book.create(book_params)
+    authors = author_params[:name].split(",")
+    authors.each do |author|
+      book.authors.create(name: author)
+    end
+   redirect_to book_path(book.id)
+  end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :year, :pages, :authors, :image_url)
+    retianed_params = params.require(:book).permit(:title, :year, :pages, :image_url, :authors)
+    {title: retianed_params[:title], year: retianed_params[:year], pages: retianed_params[:pages], image_url: retianed_params[:image_url]}
   end
+
+  def author_params
+    retianed_params = params.require(:book).permit(:authors)
+    {name: retianed_params[:authors]}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'books#index'
-  resources :books, only:[:index, :show, :new, :create] do
-    resources :authors, shallow: true 
-  end
+
+  resources :books, only:[:index, :show, :new, :create]
 
   resources :authors, only:[:show]
 
